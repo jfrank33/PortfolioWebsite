4a063763b523300dea007051f11f459906336246
jnfrank <jnfrank>
2020-09-27 17:27:46 -0500

    First commit. Includes vue-bootstrap tabs, images of hobbies, practice with css and general application build setup.

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h1 id = "header">
+      <strong>Jenn Frank</strong>
+      </h1>
+      <b-card>
+    <b-tabs card align="center">
+      <b-tab title="About Me">
+        <AboutMe />
+      </b-tab>
+      <b-tab title="Portfolio">
+        <Portfolio />
+      </b-tab>
+      <b-tab title="Resume">
+        <h1>Embedded Resume PDF</h1>
+        <!--<embed src="./assets/img/FrankResume.pdf">-->
+      </b-tab>
+    </b-tabs>
+  </b-card>
+  </div>
+</template>
+
+<script>
+import Portfolio from './components/Portfolio.vue'
+import AboutMe from './components/AboutMe.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Portfolio,
+    AboutMe
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+
+#header{
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  color: #890928;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hobby_imgs">
+    <b-container fluid >
+      <b-row>
+        <b-col>
+                <img width="95%" height="90%" class="image" thumbnail fluid src="../assets/img/Skiing.png">
+                <p class="img_description">Skiing</p>
+        </b-col>
+        <b-col>
+                <img width="95%" height="90%" thumbnail fluid src="../assets/img/Biking.png">
+                <p>Biking</p>
+        </b-col>
+            <b-col>
+                <img width="95%" height="90%" thumbnail fluid src="../assets/img/Hiking.png">
+                <p>Hiking</p>
+        </b-col>
+        
+      </b-row>
+      <b-row>
+          <b-col>
+                <img width="95%" height="90%" thumbnail fluid src="../assets/img/Kayaking.png">
+                <p>Kayaking</p>
+        </b-col>
+        <b-col>
+                <img width="95%" height="90%" thumbnail fluid src="../assets/img/Camping.png">
+                <p>Camping</p>
+        </b-col>
+              <b-col>
+                <img width="95%" height="90%" thumbnail fluid src="../assets/img/Millie.png">
+                <p>My adventure buddy, Millie</p>
+        </b-col>
+        </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style scoped>
+    /* quick reset */
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+
+</style>
