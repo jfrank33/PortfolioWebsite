fa5701fae2a2e71335d03e5b39a58314d7b8c379
jnfrank <jnfrank>
2020-10-11 03:36:39 -0500

    Accidentally put the wrong resume on. Tried to add a split screen of an image of myself and description of the webpage but the CSS was acting up for mobile, so I temporarily cut the image and just left the text.

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,6 @@ export default {
   font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 20px;
 }
 </style>
--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="hobby_imgs">  
+  <div class="AboutMe">  
     <b-container fluid >
+      <b-row>
+        <!--
+        <b-col>
+          <img id="jennimg" src="../assets/img/Jenn.png">
+        </b-col>
+        -->
+        <b-col>
+          <p id="jenntxt">Hello and welcome! My name is Jenn Frank and I am a software developer, gemini, adventurer, and plant-lover, amongst many other things.
+          As you may notice, this webpage is currently a work in progress. I am using it as a creativity outlet as well
+          as a fun way to freshen up my web development skills and learn the Vue.JS framework. I plan to use the Blog tab to 
+          document my developing progress, share recipes, document adventures, and anything else I may think of. Other tabs and features
+          may pop up as I explore Vue.js, so be on the lookout! </p> 
+        </b-col>
+      </b-row>
+      <!--
+      <b-row>
+        <h1 id="hobbiestxt"> Hobbies </h1>
+      </b-row>
+      -->
       <b-row>
         <b-col>
                 <img width="95%" height="90%" class="image" src="../assets/img/Skiing.png" v-on:click="toggleImageText">
@@ -51,11 +70,38 @@ export default {
 
 <style scoped>
 * {
-  margin: 0;
+  margin: 0 auto;
   padding: 0;
   border: 0;
-  padding-top: 10px;
+  padding-top: 0px;
   position: relative;
+  text-align: center;
+}
+#jennimg{
+  left: 0;
+  float: left; 
+  width: 100%;
+  padding-bottom: 20px;
+  margin-left: -50%;
+  position: fixed;
+}
+#jenntxt{
+ width: 100%;
+ height: 100%;
+ text-align: center;
+ display: inline-block;
+ width: 85%;
+ height: 100%;
+ margin-right: -4px;
+ padding-top: 20px;
+}
+.hobby_imgs{
+  position: relative;
+  padding-bottom: 20px;
+}
+
+#hobbiestxt{
+  justify-content: center;
 }
 
 </style>
